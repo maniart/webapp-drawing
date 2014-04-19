be608ee0f8b998ee0dfdbc652d9941808054f699
Mani Nilchiani <mani.art@gmail.com>
2014-04-19 13:02:08 -0400

    init commit and simple face detection example

---
+++ experiments/simple-face-detection/index.js
@@ -0,0 +1,21 @@
+;(function(exp){
+
+	var cv;
+	cv = require('opencv');
+	cv.readImage(__dirname + '/image.jpg', function(error, image) {
+		image.detectObject(cv.FACE_CASCADE, {}, function(error, faces){
+			var i = 0,
+				len = faces.length,
+				face;
+			for(i; i < len; i += 1) {
+				face = faces[i];
+				image.ellipse(face.x + face.width/2, face.y + face.height/2, face.width/2, face.height/2);
+			}
+			image.save('./out.jpg');
+		});
+	});
+
+
+}(exports));
+
+
